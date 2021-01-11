74c41b6789cf35dddfaadf765792dec9deca4773
Hanshi-Obra <66540964+Hanshi-Obra@users.noreply.github.com>
2021-01-10 20:28:35 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,289 @@
+import * as React from 'react';
+import {
+  StatusBar,
+  Dimensions,
+  TouchableOpacity,
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  Image
+} from 'react-native';
+import Constants from 'expo-constants';
+import { AntDesign } from '@expo/vector-icons';
+const { width } = Dimensions.get('window');
+
+const AnimatedAntDesign = Animated.createAnimatedComponent(AntDesign);
+
+const DURATION = 1000;
+const TEXT_DURATION = DURATION * 0.8;
+
+const quotes = [
+  {
+    quote:
+      'For the things we have to learn before we can do them, we learn by doing them.',
+    image:'https://img.archiexpo.es/images_ae/photo-g/158659-13673307.jpg'
+  },
+  {
+    quote: 'The fastest way to build an app.',
+    image:'https://images-na.ssl-images-amazon.com/images/I/31eOWETPtQL._AC_.jpg'
+  },
+  {
+    quote:
+      'The greatest glory in living lies not in never falling, but in rising every time we fall.',
+    image:'https://image.freepik.com/foto-gratis/verde-claro-pintado-color-liso-transparente-texturado-enyesado-fondo-pared-aspera_46187-465.jpg'
+  },
+  
+];
+
+
+
+const Circle = ({ onPress, index, quotes, animatedValue, animatedValue2 }) => {
+  const { initialBgColor, nextBgColor, bgColor } = colors[index];
+  const inputRange = [0, 0.001, 0.5, 0.501, 1];
+  const backgroundColor = animatedValue2.interpolate({
+    inputRange,
+    outputRange: [
+      initialBgColor,
+      initialBgColor,
+      initialBgColor,
+      bgColor,
+      bgColor,
+    ],
+  });
+  const dotBgColor = animatedValue2.interpolate({
+    inputRange: [0, 0.001, 0.5, 0.501, 0.9, 1],
+    outputRange: [
+      bgColor,
+      bgColor,
+      bgColor,
+      initialBgColor,
+      initialBgColor,
+      nextBgColor,
+    ],
+  });
+
+  return (
+    <Animated.View
+      style={[
+        StyleSheet.absoluteFillObject,
+        styles.container,
+        { backgroundColor },
+      ]}
+    >
+      <Animated.View
+        style={[
+          styles.circle,
+          {
+            backgroundColor: dotBgColor,
+            transform: [
+              { perspective: 300 },
+              {
+                rotateY: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: ['0deg', '-90deg', '-180deg'],
+                }),
+              },
+
+              {
+                scale: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: [1, 6, 1],
+                }),
+              },
+
+              {
+                translateX: animatedValue2.interpolate({
+                  inputRange: [0, 0.5, 1],
+                  outputRange: ['0%', '50%', '0%'],
+                }),
+              },
+            ],
+          },
+        ]}
+      >
+        <TouchableOpacity onPress={onPress}>
+          <Animated.View
+            style={[
+              styles.button,
+              {
+                transform: [
+                  {
+                    scale: animatedValue.interpolate({
+                      inputRange: [0, 0.05, 0.5, 1],
+                      outputRange: [1, 0, 0, 1],
+                      // extrapolate: "clamp"
+                    }),
+                  },
+                  {
+                    rotateY: animatedValue.interpolate({
+                      inputRange: [0, 0.5, 0.9, 1],
+                      outputRange: ['0deg', '180deg', '180deg', '180deg'],
+                    }),
+                  },
+                ],
+                opacity: animatedValue.interpolate({
+                  inputRange: [0, 0.05, 0.9, 1],
+                  outputRange: [1, 0, 0, 1],
+                }),
+              },
+            ]}
+          >
+            <AnimatedAntDesign name='arrowright' size={30} color={'#aa00c7'} />
+          </Animated.View>
+        </TouchableOpacity>
+      </Animated.View>
+    </Animated.View>
+  );
+};
+
+/* 
+initialBgColor -> Big background of the element
+bgColor -> initial circle bg color that will be the next slide initial BG Color
+nextBgColor -> next circle bg color after we fully transition the circle and this will be small again
+prev bgColor === next initialBgColor
+prev nextBgColor === next bgColor
+*/
+
+const colors = [
+  {
+    initialBgColor: 'black',
+    bgColor: 'black',
+    nextBgColor: 'white',
+  },
+  
+  {
+    initialBgColor: 'black',
+    bgColor: 'white',
+    nextBgColor: 'black',
+  },
+  {
+    initialBgColor: 'white',
+    bgColor: 'black',
+    nextBgColor: 'white',
+  },
+  {
+    initialBgColor: 'black',
+    bgColor: 'white',
+    nextBgColor: 'black',
+  },
+];
+
+export default function App() {
+  const animatedValue = React.useRef(new Animated.Value(0)).current;
+  const animatedValue2 = React.useRef(new Animated.Value(0)).current;
+  const sliderAnimatedValue = React.useRef(new Animated.Value(0)).current;
+  const inputRange = [...Array(quotes.length).keys()];
+  const [index, setIndex] = React.useState(0);
+
+  const animate = (i) =>
+    Animated.parallel([
+      Animated.timing(sliderAnimatedValue, {
+        toValue: i,
+        duration: TEXT_DURATION,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animatedValue, {
+        toValue: 1,
+        duration: DURATION,
+        useNativeDriver: true,
+      }),
+      Animated.timing(animatedValue2, {
+        toValue: 1,
+        duration: DURATION,
+        useNativeDriver: false,
+      }),
+    ]);
+
+  const onPress = () => {
+    animatedValue.setValue(0);
+    animatedValue2.setValue(0);
+    animate((index + 1) % colors.length).start();
+    setIndex((index + 1) % colors.length);
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'flex-start', paddingTop: 100 }}>
+      <StatusBar hidden />
+      <Circle
+        index={index}
+        onPress={onPress}
+        quotes={quotes}
+        animatedValue={animatedValue}
+        animatedValue2={animatedValue2}
+      />
+      <Animated.View
+        style={{
+          flexDirection: 'row',
+          transform: [
+            {
+              translateX: sliderAnimatedValue.interpolate({
+                inputRange,
+                outputRange: quotes.map((_,i) => -i * width * 2),
+              }),
+            },
+          ],
+          opacity: sliderAnimatedValue.interpolate({
+            inputRange: [...Array(quotes.length * 2 + 1).keys()].map(
+              (i) => i / 2
+            ),
+            outputRange: [...Array(quotes.length * 2 + 1).keys()].map((i) =>
+              i % 2 === 0 ? 1 : 0
+            ),
+          }),
+        }}
+      >
+        {quotes.slice(0, colors.length).map(({ quote, image }, i) => {
+          return (
+            <View style={{ paddingRight: width, width: width * 2 , justifyContent:'center', alignItems:'center'}} key={i}>
+              <Text
+                style={[styles.paragraph, { color: colors[i].nextBgColor }]}
+              >
+                {quote}
+              </Text>
+               <Image source={{uri:image}} style={{width:'60%', height:'40%', borderRadius:20 }} resizeMode='cover'   />
+            </View>
+          );
+        })}
+      </Animated.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingTop: Constants.statusBarHeight,
+    padding: 8,
+    paddingBottom: 50,
+  },
+  paragraph: {
+    margin: 12,
+    fontSize: 24,
+    // fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: 'Menlo',
+    color: 'white',
+  },
+  button: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor:'black'
+  },
+  circle: {
+    backgroundColor: 'turquoise',
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    shadowColor:'gray',
+    shadowOpacity:1,
+    shadowRadius:20,
+    elevation:10
+    
+  },
+});
